a4c49f50082f5594c41cc9a8d17fcf5547fcbeb4
hasyee <hasyee@gmail.com>
2020-02-23 00:38:25 +0100

    Revert "remove useCallback-s"
    This reverts commit 62f1606ce8fe1032fb99beb2d30b12fd987dafe5.

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Calendar } from 'react-calendar-component';
 import { Button } from '@blueprintjs/core';
 import moment from 'moment';
@@ -6,25 +6,29 @@ import { useDate } from '../store';
 import Location from './Location';
 import './Calendar.scss';
 
-export default React.memo(() => {
+export default () => {
   const [date, setDate] = useDate();
+
+  const handleMonthChange = useCallback(date => setDate(date.valueOf()), [setDate]);
+  const handleDatePick = useCallback(date => console.log(date), []);
+  const handleSetToday = useCallback(() => setDate(Date.now()), [setDate]);
 
   return (
     <Calendar
       date={moment(date)}
-      onChangeMonth={date => setDate(date.valueOf())}
-      onPickDate={date => console.log(date)}
+      onChangeMonth={handleMonthChange}
+      onPickDate={handleDatePick}
       renderHeader={({ date, onPrevMonth, onNextMonth }) => (
         <div className="Calendar-header">
           <Location />
           <div className="date-controls">
             <div className="Calendar-header-currentDate">{date.format('MMMM YYYY')}</div>
             <Button onClick={onPrevMonth}>«</Button>
-            <Button onClick={() => setDate(Date.now())}>TODAY</Button>
+            <Button onClick={handleSetToday}>TODAY</Button>
             <Button onClick={onNextMonth}>»</Button>
           </div>
         </div>
       )}
     />
   );
-});
+};
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Button, NumericInput, FormGroup } from '@blueprintjs/core';
 import { useLocation } from '../store';
 import './Location.scss';
 
-export default React.memo(() => {
+const Location = () => {
   const [location, setLocation] = useLocation();
   const [lng, setLng] = useState(location[0]);
   const [lat, setLat] = useState(location[1]);
+  const handleSubmit = useCallback(() => setLocation([lng, lat]), [setLocation, lng, lat]);
 
   return (
     <div className="Location">
@@ -16,7 +17,9 @@ export default React.memo(() => {
       <FormGroup label="Lng" inline className="form-group">
         <NumericInput value={lng} onValueChange={setLng} min={-180} max={+180} />
       </FormGroup>
-      <Button onClick={() => setLocation([lng, lat])}>SUBMIT</Button>
+      <Button onClick={handleSubmit}>SUBMIT</Button>
     </div>
   );
-});
+};
+
+export default Location;
