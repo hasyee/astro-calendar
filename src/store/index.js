6c672fc404708670b6a87efe2100fe5f4e38f4fa
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2020-02-22 21:01:15 +0100

    init commit

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import { useMemo } from 'react';
+import Store from 'repatch';
+import { bindActionCreators } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from './actions';
+export * from './selectors';
+
+const store = new Store({
+  date: Date.now(),
+  location: [19, 47]
+});
+
+window.store = store;
+
+export default store;
+
+export const useActions = () => {
+  const dispatch = useDispatch();
+  return useMemo(() => {
+    return Object.keys(actions).reduce(
+      (acc, actionName) => ({ ...acc, [actionName]: bindActionCreators(actions[actionName], dispatch) }),
+      {}
+    );
+  }, [dispatch]);
+};
