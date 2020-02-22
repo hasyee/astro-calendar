62f1606ce8fe1032fb99beb2d30b12fd987dafe5
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2020-02-22 23:52:29 +0100

    remove useCallback-s

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,13 +1,12 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import { Button, NumericInput, FormGroup } from '@blueprintjs/core';
 import { useLocation } from '../store';
 import './Location.scss';
 
-const Location = () => {
+export default React.memo(() => {
   const [location, setLocation] = useLocation();
   const [lng, setLng] = useState(location[0]);
   const [lat, setLat] = useState(location[1]);
-  const handleSubmit = useCallback(() => setLocation([lng, lat]), [setLocation, lng, lat]);
 
   return (
     <div className="Location">
@@ -17,9 +16,7 @@ const Location = () => {
       <FormGroup label="Latitude" inline className="form-group">
         <NumericInput value={lat} onValueChange={setLat} min={-90} max={+90} />
       </FormGroup>
-      <Button onClick={handleSubmit}>SUBMIT</Button>
+      <Button onClick={() => setLocation([lng, lat])}>SUBMIT</Button>
     </div>
   );
-};
-
-export default Location;
+});
