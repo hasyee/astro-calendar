8b39bd44e239f82de1256f93a83c1c6b27a93f79
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2020-02-23 13:41:41 +0100

    wip

--- src/components/Night.js
+++ src/components/Night.js
@@ -2,8 +2,16 @@ import React from 'react';
 import moment from 'moment';
 import './Night.scss';
 
+const f = interval => {
+  if (!interval) return;
+  return {
+    start: moment(interval.start).format('DD HH:mm'),
+    end: moment(interval.end).format('DD HH:mm')
+  };
+};
+
 export default React.memo(function Night({ night, moonNight, astroNight, moonlessNight }) {
-  console.log({ night, moonNight, astroNight, moonlessNight });
+  console.log({ night: f(night), moonNight: f(moonNight), astroNight: f(astroNight), moonlessNight: f(moonlessNight) });
   return (
     <div className="Night">
       <div className="daylight" />
