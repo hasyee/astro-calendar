79efc3a2eae15ad2f63fc2efd371eb6cfb75deaa
hasyee <hasyee@gmail.com>
2020-02-23 11:36:46 +0100

    move location settings to dialog

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Calendar as CalendarComponent } from 'react-calendar-component';
 import { Button } from '@blueprintjs/core';
 import moment from 'moment';
@@ -9,7 +9,9 @@ import './Calendar.scss';
 export default React.memo(function Calendar() {
   const date = useSelector(getDate);
   const { setDate } = useActions();
-
+  const [isOpenLocationDialog, setIsOpenLocationDialog] = useState(false);
+  const handleOpenLocationDialog = useCallback(() => setIsOpenLocationDialog(true), []);
+  const handleCloseLocationDialog = useCallback(() => setIsOpenLocationDialog(false), []);
   const handleMonthChange = useCallback(date => setDate(date.valueOf()), [setDate]);
   const handleDatePick = useCallback(date => console.log(date), []);
   const handleSetToday = useCallback(() => setDate(Date.now()), [setDate]);
@@ -21,7 +23,10 @@ export default React.memo(function Calendar() {
       onPickDate={handleDatePick}
       renderHeader={({ date, onPrevMonth, onNextMonth }) => (
         <div className="Calendar-header">
-          <Location />
+          <Button icon="locate" onClick={handleOpenLocationDialog}>
+            Location
+          </Button>
+          <Location isOpen={isOpenLocationDialog} onClose={handleCloseLocationDialog} />
           <div className="date-controls">
             <div className="Calendar-header-currentDate">{date.format('MMMM YYYY')}</div>
             <Button onClick={onPrevMonth}>«</Button>
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,24 +1,33 @@
 import React, { useState, useCallback } from 'react';
-import { Button, NumericInput, FormGroup } from '@blueprintjs/core';
+import { Button, NumericInput, FormGroup, Dialog, Classes } from '@blueprintjs/core';
 import { useSelector, useActions, getLocation } from '../store';
 import './Location.scss';
 
-export default React.memo(function Location() {
+export default React.memo(function Location({ isOpen, onClose }) {
   const location = useSelector(getLocation);
   const { setLocation } = useActions();
   const [lng, setLng] = useState(location[0]);
   const [lat, setLat] = useState(location[1]);
-  const handleSubmit = useCallback(() => setLocation([lng, lat]), [setLocation, lng, lat]);
+  const handleSubmit = useCallback(() => {
+    setLocation([lng, lat]);
+    onClose();
+  }, [setLocation, onClose, lng, lat]);
 
   return (
-    <div className="Location">
-      <FormGroup label="Lat" inline className="form-group">
-        <NumericInput value={lat} onValueChange={setLat} min={-90} max={+90} className="input" />
-      </FormGroup>
-      <FormGroup label="Lng" inline className="form-group">
-        <NumericInput value={lng} onValueChange={setLng} min={-180} max={+180} className="input" />
-      </FormGroup>
-      <Button onClick={handleSubmit}>OK</Button>
-    </div>
+    <Dialog icon="locate" title="Location" isOpen={isOpen} onClose={onClose}>
+      <div className={Classes.DIALOG_BODY}>
+        <FormGroup label="Lat" inline className="form-group">
+          <NumericInput value={lat} onValueChange={setLat} min={-90} max={+90} />
+        </FormGroup>
+        <FormGroup label="Lng" inline className="form-group">
+          <NumericInput value={lng} onValueChange={setLng} min={-180} max={+180} />
+        </FormGroup>
+      </div>
+      <div className={Classes.DIALOG_FOOTER}>
+        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+          <Button onClick={handleSubmit}>OK</Button>
+        </div>
+      </div>
+    </Dialog>
   );
 });
