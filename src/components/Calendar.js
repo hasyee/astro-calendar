7811b8818ed604ef728df18f6ae28cae1b398a53
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2020-02-24 00:28:55 +0100

    remove cache

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import useCache from '../hooks/cache';
 import CalendarItem from './CalendarItem';
+import { useSelector, getDays } from '../store';
 import './Calendar.scss';
 
 export default React.memo(function Calendar() {
-  const days = useCache();
-  
+  const days = useSelector(getDays);
+
   return (
     <div className="Calendar">
       <div className="grid">
