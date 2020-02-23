41ba90b2effcec3136b7895f42d054cbba47bb75
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2020-02-23 16:39:33 +0100

    lint fixes

--- src/components/Night.js
+++ src/components/Night.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import moment from 'moment';
 import './Night.scss';
-
-const f = interval => {
-  if (!interval) return;
-  return {
-    start: moment(interval.start).format('DD HH:mm'),
-    end: moment(interval.end).format('DD HH:mm')
-  };
-};
 
 export default React.memo(function Night({ night, moonNight, astroNight, moonlessNight }) {
   const renderBand = (name, band, i) => (
