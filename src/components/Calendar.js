83ea4e1dd49c8d6310149374334af1986e0b2a9d
hasyee <hasyee@gmail.com>
2020-02-23 16:39:33 +0100

    lint fixes

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { useSelector, getDays } from '../store';
 import CalendarItem from './CalendarItem';
 import './Calendar.scss';
