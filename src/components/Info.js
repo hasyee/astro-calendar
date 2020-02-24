08ee52baaa9da4ccc2393006bd86f6d01064b975
hasyee <hasyee@gmail.com>
2020-02-24 14:17:27 +0100

    improve Info and responsivity

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,19 +5,19 @@ import './Info.scss';
 
 export default React.memo(function Info(info) {
   const renderTransit = (name, interval, which) => (
-    <Fragment>
+    <Fragment key={which}>
       <td>{name}</td>
-      <td>{interval && Number.isFinite(interval[which]) ? moment(interval[which]).format('HH:mm') : '-'}</td>
+      <td>{interval && Number.isFinite(interval[which]) ? moment(interval[which]).format('ddd HH:mm') : '-'}</td>
     </Fragment>
   );
 
-  const renderRow = (name, key, start, end) => (
+  const renderRow = (name, key, startName, endName) => (
     <tr>
       <td>
         <span className={classnames('dot', key)} /> {name}
       </td>
-      {renderTransit(start, info[key], 'start')}
-      {renderTransit(end, info[key], 'end')}
+      {renderTransit(startName, info[key], 'start')}
+      {renderTransit(endName, info[key], 'end')}
     </tr>
   );
 
@@ -28,7 +28,7 @@ export default React.memo(function Info(info) {
         <tbody>
           {renderRow('Night', 'night', 'Sunset', 'Sunrise')}
           {renderRow('Astro night', 'astroNight', 'From', 'To')}
-          {renderRow('Moon', 'moonNight', 'Moonrise', 'Moonset')}
+          {renderRow('Moon', 'moonNight', 'Moonset', 'Moonrise')}
           {renderRow('Moonless night', 'moonlessNight', 'From', 'To')}
         </tbody>
       </table>
