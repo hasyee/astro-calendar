fe33d884115e8fa25ba68b39090beba15ebdd8a6
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2020-02-27 17:19:38 +0100

    worker

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import Calendar from './Calendar';
-import { useLocalStorage } from '../hooks';
+import { useLocalStorage, useWorker } from '../hooks';
 import './App.scss';
 
 export default React.memo(function App() {
   useLocalStorage();
+  useWorker();
 
   return (
     <div className="App">
