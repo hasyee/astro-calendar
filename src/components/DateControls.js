4b3eb6473e2164fce77ba40899e5deb5e5d6e270
hasyee <hasyee@gmail.com>
2020-03-07 19:46:35 +0100

    DateControl component, fixes

---
+++ src/components/DateControls.js
@@ -0,0 +1,44 @@
+import React, { useCallback } from 'react';
+import moment from 'moment';
+import { Button } from '@blueprintjs/core';
+import { useDate } from '../hooks';
+import './DateControl.scss';
+
+export default React.memo(function DateControls() {
+  const [date, setDate] = useDate();
+  const updateMonth = useCallback(
+    value =>
+      setDate(
+        moment(date)
+          .add(value, 'months')
+          .valueOf()
+      ),
+    [date, setDate]
+  );
+  const handlePrevMonth = useCallback(() => updateMonth(-1), [updateMonth]);
+  const handleNextMonth = useCallback(() => updateMonth(+1), [updateMonth]);
+  const handleThisMonth = useCallback(
+    () =>
+      setDate(
+        moment()
+          .startOf('day')
+          .valueOf()
+      ),
+    [setDate]
+  );
+
+  return (
+    <div className="DateControls">
+      <div className="current-date">{moment(date).format('MMMM YYYY')}</div>
+      <Button onClick={handlePrevMonth} large>
+        «
+      </Button>
+      <Button onClick={handleThisMonth} large>
+        •
+      </Button>
+      <Button onClick={handleNextMonth} large>
+        »
+      </Button>
+    </div>
+  );
+});
