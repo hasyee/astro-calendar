55afd4fb71f7493e8ee4d8489fdedb8632f9ed5b
hasyee <hasyee@gmail.com>
2020-03-08 16:27:06 +0100

    io.state

--- src/hooks/state.js
+++ src/hooks/state.js
@@ -1,19 +1,19 @@
 import moment from 'moment';
 import io from 'use.io';
 
-export const useDate = io(
+export const useDate = io.state(
   moment()
     .startOf('month')
     .valueOf()
 );
 
-export const useLng = io(19);
+export const useLng = io.state(19);
 
-export const useLat = io(47);
+export const useLat = io.state(47);
 
 export const useCoords = io.compose({ lng: useLng, lat: useLat });
 
-export const useLocationName = io('');
+export const useLocationName = io.state('');
 
 export const useLocation = io.compose({ coords: useCoords, name: useLocationName }, io.deepMergeUpdater);
 
@@ -28,4 +28,4 @@ export const useLocationShortName = io.select(
   [useLocation]
 );
 
-export const useDays = io([]);
+export const useDays = io.state([]);
